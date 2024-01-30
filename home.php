3cc8da95447455b7372771b1256f8544aab69dcf
Adam <adam.rijnbeek@student.rocva.nl>
2024-01-30 14:11:43 +0100

    Header Commit

--- home.php
+++ home.php
@@ -14,16 +14,16 @@
 <body>
   <section class="header">
     
-    <a href="home.php" class="logo">Travel.</a>
+    <a href="home.php" class="logo">Travel & Tour</a>
 
 <nav class="navbar">
       <a href="home.php">home</a>
       <a href="about.php">about</a>
       <a href="package.php">package</a>
       <a href="book.php">book</a>
-      
 </nav>
 
+<div id="menu-btn" class="fas fa-bars"></div>
 
 
 
