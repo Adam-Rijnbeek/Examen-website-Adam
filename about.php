8316c4b243957ce19b89684656327191d3e6d590
Adam <adam.rijnbeek@student.rocva.nl>
2024-01-30 15:49:41 +0100

    Footer Commit

---
+++ about.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/css/lg-fb-comment-box.min.css">
+
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+</head>
+<body>
+  <section class="header">
+    
+    <a href="home.php" class="logo">Travel & Tour</a>
+
+<nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+</nav>
+
+<div id="menu-btn" class="fas fa-bars"></div>
+</section>
+
+
+<div class="heading" style="background:url(images/hiking-man-with-backpack-walking-forest.jpg) no-repeat">
+<h1>about us</h1>
+</div>
+
+
+
+
+
+<section class="footer">
+
+<div class="box-container">
+
+<div class="box">
+    <h3>quick links</h3>
+    <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>"
+    <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>"
+    <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>"
+    <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>"
+</div>
+
+<div class="box">
+    <h3>extra links</h3>
+    <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>"
+    <a href="#"> <i class="fas fa-angle-right"></i> about us</a>"
+    <a href="#"> <i class="fas fa-angle-right"></i> privacy</a>"
+    <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>"
+</div>
+
+<div class="box">
+    <h3>contact info</h3>
+    <a href="#"> <i class="fas fa-phone"></i> 0612345678</a>"
+    <a href="#"> <i class="fas fa-phone"></i> 0698765432</a>"
+    <a href="#"> <i class="fas fa-envelope"></i> travelentour@gmail.com</a>"
+    <a href="#"> <i class="fas fa-map"></i> Uithoorn, Netherlands - Witkopeend 13</a>"
+</div>
+
+<div class="box">
+    <h3>follow us</h3>
+    <a href="#"> <i class="fas fa-twitter"></i> twitter</a>"
+    <a href="#"> <i class="fas fa-instagram"></i> instagram</a>"
+    <a href="#"> <i class="fas fa-facebook"></i> facebook</a>"
+</div>
+</div>
+<div class="credit"> created by <div><span> Adam and Collin</span></div> | all rights reserved | </div>
+
+</section>
+
+
+
+
+
+
+
+
+    <script src="script.js"></script>
+</body>
+</html>
--- home.php
+++ home.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travel&Tour Adam</title>
+    <title>Home</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/css/lg-fb-comment-box.min.css">
 
     <link rel="stylesheet" href="style.css">
 
@@ -24,8 +26,46 @@
 </nav>
 
 <div id="menu-btn" class="fas fa-bars"></div>
+</section>
 
+<section class="footer">
 
+<div class="box-container">
+
+<div class="box">
+    <h3>quick links</h3>
+    <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>"
+    <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>"
+    <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>"
+    <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>"
+</div>
+
+<div class="box">
+    <h3>extra links</h3>
+    <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>"
+    <a href="#"> <i class="fas fa-angle-right"></i> about us</a>"
+    <a href="#"> <i class="fas fa-angle-right"></i> privacy</a>"
+    <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>"
+</div>
+
+<div class="box">
+    <h3>contact info</h3>
+    <a href="#"> <i class="fas fa-phone"></i> 0612345678</a>"
+    <a href="#"> <i class="fas fa-phone"></i> 0698765432</a>"
+    <a href="#"> <i class="fas fa-envelope"></i> travelentour@gmail.com</a>"
+    <a href="#"> <i class="fas fa-map"></i> Uithoorn, Netherlands - Witkopeend 13</a>"
+</div>
+
+<div class="box">
+    <h3>follow us</h3>
+    <a href="#"> <i class="fas fa-twitter"></i> twitter</a>"
+    <a href="#"> <i class="fas fa-instagram"></i> instagram</a>"
+    <a href="#"> <i class="fas fa-facebook"></i> facebook</a>"
+</div>
+</div>
+<div class="credit"> created by <div><span> Adam and Collin</span></div> | all rights reserved | </div>
+
+</section>
 
 
 
